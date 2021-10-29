54e0def9b0417425b5bbe3c807cc95e227ba41a1
42void <al.vanhoegaerden@gmail.com>
2021-10-29 21:53:00 +0200

    reindent

--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -1,21 +1,21 @@
 import React from 'react'
 
 export const Pagination = ({paginationLimit, pageNumber, numberOfPages, previousPage, handleLimitChange, nextPage}) => {
-    return (
-        <>
-            <div className="pagination-container">
-                <div className="pagination-navigation">
-                    <div className="pagination-previous" onClick={()=>previousPage()}>{"<"} Précédent</div>
-                    <select value={paginationLimit} onChange={(e)=>handleLimitChange(e)}>
-                        <option defaultValue="4">4</option>
-                        <option value="8">8</option>
-                        <option value="12">12</option>
-                    </select>
-                    <div className="pagination-next" onClick={()=>nextPage()}>Suivant {">"}</div>
-                </div>
-                <div className="pagination-page-number">Page {pageNumber + 1}/{numberOfPages}</div>
-            </div>
-        </>
-    )
+	return (
+		<>
+			<div className="pagination-container">
+				<div className="pagination-navigation">
+					<div className="pagination-previous" onClick={()=>previousPage()}>{"<"} Précédent</div>
+					<select value={paginationLimit} onChange={(e)=>handleLimitChange(e)}>
+						<option defaultValue="4">4</option>
+						<option value="8">8</option>
+						<option value="12">12</option>
+					</select>
+					<div className="pagination-next" onClick={()=>nextPage()}>Suivant {">"}</div>
+				</div>
+				<div className="pagination-page-number">Page {pageNumber + 1}/{numberOfPages}</div>
+			</div>
+		</>
+	)
 }
     
